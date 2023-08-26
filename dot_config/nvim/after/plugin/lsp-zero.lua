cfc671f96b357e45fa84719b6b38eb9a41f4faf2
odovren <odovren@proton.me>
2023-08-26 03:32:14 +0200

    improve autocomplete for nvim

--- dot_config/nvim/after/plugin/lsp-zero.lua
+++ dot_config/nvim/after/plugin/lsp-zero.lua
@@ -1,5 +1,16 @@
 local lsp = require('lsp-zero').preset({})
 
+lsp.set_preferences({
+  suggest_lsp_servers = true,
+  sign_icons = {
+    error = "E",
+    warn = "W",
+    hint = "H",
+    info = "I"
+  }
+})
+
+---@diagnostic disable-next-line: unused-local
 lsp.on_attach(function(client, bufnr)
   -- see :help lsp-zero-keybindings
   -- to learn the available actions
@@ -13,8 +24,20 @@ lsp.on_attach(function(client, bufnr)
   vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
   vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
 end)
-
 -- (Optional) Configure lua language server for neovim
 require('lspconfig').lua_ls.setup(lsp.nvim_lua_ls())
 
 lsp.setup()
+
+local cmp = require('cmp')
+
+cmp.setup({
+  mapping = {
+    -- `Enter` key to confirm completion
+    ['<CR>'] = cmp.mapping.confirm({select = true}),
+
+    -- Ctrl+Space to trigger completion menu
+    ['<C-Space>'] = cmp.mapping.complete(),
+  }
+})
+
